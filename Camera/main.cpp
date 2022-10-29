52513eeeb972d619db455630dbe5ce7df9f777bf
jinjo <creationer625@outlook.com>
2022-10-29 13:19:54 +0800

    Initial Commit

---
+++ Camera/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Camera
+//
+//  Created by ShuYang Yan on 2022/10/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
